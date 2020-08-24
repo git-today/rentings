b9e10dd75b9cfae0d6c9c71da8bc3b7dd0a2fa69
Your Name“git config --global user.name Your <553657333@qq.com>
2020-08-24 17:57:56 +0800

    天气2

--- src/main/java/com/aaa/controller/WatherController.java
+++ src/main/java/com/aaa/controller/WatherController.java
@@ -2,21 +2,23 @@ package com.aaa.controller;
 
 import com.aaa.util.wather.MyWatherUtil;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.net.SocketException;
 @CrossOrigin
-@RestController
+@Controller
 @RequestMapping(value = "wather",produces = { "application/json;charset=UTF-8" })
 public class WatherController {
-
+    @ResponseBody
     @RequestMapping("getWather")
-    public Object getWather() throws SocketException {
+    public String getWather() throws SocketException {
         MyWatherUtil myWatherUtil = new MyWatherUtil();
-        JSONObject jsonObject = JSONObject.parseObject(myWatherUtil.getWather());
-        return jsonObject;
+        return myWatherUtil.getWather();
     }
+
 }
