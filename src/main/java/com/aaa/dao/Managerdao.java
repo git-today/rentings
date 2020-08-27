28f2ffe06d21f6a531896723e6b2ca17559d124b
wang <2041409629@qq.com>
2020-08-27 10:00:56 +0800

    w-8,27

--- src/main/java/com/aaa/dao/Managerdao.java
+++ src/main/java/com/aaa/dao/Managerdao.java
@@ -3,12 +3,16 @@ package com.aaa.dao;
 import com.aaa.entity.Manager;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.web.bind.annotation.Mapping;
 
 import java.util.List;
 
 @Mapper
 public interface Managerdao extends tk.mybatis.mapper.common.Mapper<Manager> {
+
+    @Update("update manager set mgpwd=#{mgpwd} where mgid=#{mgid} ")
+    int updateNamePwd(String mgpwd,Integer mgid);
 
     @Select("select * from manager where mgname=#{mgname} and mgpwd=mgpwd ")
     List<Manager> queryNamePwd(String mgname, String mgpwd);
